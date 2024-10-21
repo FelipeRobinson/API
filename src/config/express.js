cb160e4d1e91ca684eade9c9c082301d348f886b
Felipe Spider <feliperobinsonprofissional@gmail.com>
2024-10-21 20:10:49 -0300

    Update express.js

--- src/config/express.js
+++ src/config/express.js
@@ -1,5 +1,9 @@
+const cors = require("cors")
 const express = require("express");
 const aplicacao = express();
+
+// aplicação usaraá o CORS
+aplicacao.use(cors());
 
 // configurando possibilidade de uso de jso na aplicação
 aplicacao.use(express.json());
